2de6055f7fe908ee3743af1ba2e1dc7585792058
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-05-02 10:27:12 +0700

    File items dan skill yang hilang

---
+++ 0706012110054-Fadhil-AFL1/Items.swift
@@ -0,0 +1,19 @@
+//
+//  Items.swift
+//  0706012110054-Fadhil-AFL1
+//
+//  Created by MacBook Pro on 02/05/23.
+//
+
+import Foundation
+class Items{
+var name: String
+var description: String
+var amount: Int
+    
+    init(name: String, description: String, amount: Int) {
+        self.name = name
+        self.description = description
+        self.amount = amount
+    }
+}
---
+++ 0706012110054-Fadhil-AFL1/Skill.swift
@@ -0,0 +1,19 @@
+//
+//  Skill.swift
+//  0706012110054-Fadhil-AFL1
+//
+//  Created by MacBook Pro on 02/05/23.
+//
+
+import Foundation
+class Skill{
+    var skill_name: String
+    var skill_usage: Int
+    var description: String
+    
+    init(skill_name: String, skill_usage: Int, description: String) {
+        self.skill_name = skill_name
+        self.skill_usage = skill_usage
+        self.description = description
+    }
+}
